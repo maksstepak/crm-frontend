ea2a9e930be2fd82d97dd98a601ce4c8118f835a
maksstepak <maks.stepak1@gmail.com>
2023-10-08 21:39:00 +0200

    Improve folder structure and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileComponent } from './features/profile/components/profile/profile.component';
-import { LoginComponent } from './features/login/components/login/login.component';
-import { HomeComponent } from './features/home/components/home/home.component';
+import { ProfileComponent } from './features/profile/pages/profile/profile.component';
+import { LoginComponent } from './features/login/pages/login/login.component';
+import { HomeComponent } from './features/home/pages/home/home.component';
 import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
